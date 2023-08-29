54ef2e6dda53bec4d8d7b936591c75e6e7222bbd
AmirNasser909 <amirnasser989@gmail.com>
2023-08-29 20:11:54 +0300

    - restart button - clean code

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -28,7 +29,7 @@ class ViewController: UITableViewController {
         }
         startGame()
     }
-    func startGame() {
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -57,47 +58,39 @@ class ViewController: UITableViewController {
     }
     func submit(answer: String) {
         let lowerAnswer = answer.lowercased()
-
-        let errorTitle: String
-        let errorMessage: String
-
+        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
-                    usedWords.insert(answer, at: 0)
+                    
+                    usedWords.insert(lowerAnswer, at: 0)
 
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
 
                     return
                 } else {
-                    errorTitle = "Word not recognised"
-                    errorMessage = "You can't just make them up, you know!"
+                    showErrorMessage(detail: "Word not recognised", message: "You can't just make them up, you know!")
                 }
             } else {
-                errorTitle = "Word used already"
-                errorMessage = "Be more original!"
+                showErrorMessage(detail: "Word used already", message: "Be more original!")
             }
         } else {
             guard let title = title?.lowercased() else { return }
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title)"
+            showErrorMessage(detail: "Word not possible", message: "You can't spell that word from \(title)")
+            
         }
-
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
     }
     func isPossible(word: String) -> Bool {
-        guard var tempWord = title?.lowercased() else { return false }
-
-        for letter in word {
-            if let position = tempWord.firstIndex(of: letter) {
-                tempWord.remove(at: position)
-            } else {
-                return false
-            }
-        }
+//        guard var tempWord = title?.lowercased() else { return false }
+//
+//        for letter in word {
+//            if let position = tempWord.firstIndex(of: letter) {
+//                tempWord.remove(at: position)
+//            } else {
+//                return false
+//            }
+//      }
 
         return true
     }
@@ -109,9 +102,17 @@ class ViewController: UITableViewController {
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
+        if word.count < 3 || word == title?.lowercased() {
+            return false
+        }
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
 
         return misspelledRange.location == NSNotFound
+    }
+    func showErrorMessage(detail:String, message:String ) {
+        let ac = UIAlertController(title: detail, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 }
 
